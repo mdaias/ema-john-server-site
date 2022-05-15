d6af5fe304f52654b52bde56cb1b0fcc55c84d0d
mdaias <mdaias29@gmail.com>
2022-05-15 13:51:31 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vgoc3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('emaJohn').collection('product');
+
+        //Get all products
+        app.get('/product', async(req, res) => {
+            // console.log('query',req.query)
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            // console.log(page,size)
+
+            const query = {};
+            const cursor = productCollection.find(query);
+            let products;
+            if(page || size){
+                // 0 --> skip: 0 get: 0-10(10): 
+                // 1 --> skip: 1*10 get: 11-20(10):
+                // 2 --> skip: 2*10 get: 21-30 (10):
+                // 3 --> skip: 3*10 get: 21-30 (10):
+                products = await cursor.skip(page*size).limit(size).toArray();
+            }
+            else{
+                products = await cursor.toArray();
+            }
+            
+            res.send(products);
+        });
+
+        //Total Product Count
+        app.get('/productCount', async(req, res) => {
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count});
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('ema john is running')
+});
+
+app.listen(port, () => {
+    console.log('john is running port', port);
+});
