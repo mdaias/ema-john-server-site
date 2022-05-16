182dc039a30c96ddbe25ac3714d416e5d711baad
mdaias <mdaias29@gmail.com>
2022-05-16 13:48:10 +0600

    get cart product

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -20,7 +20,7 @@ async function run() {
         const productCollection = client.db('emaJohn').collection('product');
 
         //Get all products
-        app.get('/product', async(req, res) => {
+        app.get('/product', async (req, res) => {
             // console.log('query',req.query)
             const page = parseInt(req.query.page);
             const size = parseInt(req.query.size);
@@ -29,24 +29,37 @@ async function run() {
             const query = {};
             const cursor = productCollection.find(query);
             let products;
-            if(page || size){
+            if (page || size) {
                 // 0 --> skip: 0 get: 0-10(10): 
                 // 1 --> skip: 1*10 get: 11-20(10):
                 // 2 --> skip: 2*10 get: 21-30 (10):
                 // 3 --> skip: 3*10 get: 21-30 (10):
-                products = await cursor.skip(page*size).limit(size).toArray();
+                products = await cursor.skip(page * size).limit(size).toArray();
             }
-            else{
+            else {
                 products = await cursor.toArray();
             }
-            
+
             res.send(products);
         });
 
         //Total Product Count
-        app.get('/productCount', async(req, res) => {
+        app.get('/productCount', async (req, res) => {
             const count = await productCollection.estimatedDocumentCount();
-            res.send({count});
+            res.send({ count });
+        })
+
+
+        // use post to get products by ids
+        app.post('/productByKeys', async (req, res) => {
+            const keys = req.body;
+            // console.log(keys)
+            const ids = keys.map(id => ObjectId(id));
+            // console.log(ids)
+            const query = { _id: { $in: ids } }
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
         })
     }
     finally {
